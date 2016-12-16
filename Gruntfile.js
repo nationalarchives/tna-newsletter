4975b9ae71c6071fe1e7dc1be0360e37b911561f
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-12-16 12:55:23 +0000

    Created Jquery plugin for back to origin URL

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,32 +24,9 @@ module.exports = function (grunt) {
             }
         },
         watch: {
-            scripts: {
-                files: 'js/*.js',
-                tasks: ['concat', 'uglify']
-            },
             css: {
                 files: 'css/sass/*.scss',
                 tasks: ['sass', 'cssmin']
-            }
-        },
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['js/scripts.js'],
-                dest: 'js/compiled/tna-newsletter.js'
-            }
-        },
-        uglify: {
-            options: {
-                mangle: false
-            },
-            my_target: {
-                files: {
-                    'js/compiled/tna-newsletter.min.js': ['js/compiled/tna-newsletter.js']
-                }
             }
         }
     });
@@ -57,10 +34,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify','watch']);
+    grunt.registerTask('default', ['sass', 'cssmin','watch']);
 
 };
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-var name = "Mihai";
+/*
+ * The National Archives
+ * Author:  Mihai Diaconita - WEB TEAM
+ * Newsletter Back To Origin Jquery plugin
+ * */
 
-alert(name);
+(function ($) {
+    $.fn.newsletterBackToOrigin = function (options) {
+        var settings = $.extend({}, $.fn.newsletterBackToOrigin.defaults, options);
+        return this.each(function () {
+            var thankYouURL = "http://nationalarchives.gov.uk/news/subscribe-confirmation.htm",
+                newValue = "?oldurl=" + window.location.href;
+            return settings.$element.val(thankYouURL + newValue);
+        });
+    }
+
+    // Default settings
+    $.fn.newsletterBackToOrigin.defaults = {
+        $element: $('input[name="ReturnURL"]'),
+    }
+}(jQuery));
+
+$('#signup').newsletterBackToOrigin();
