a00971e5e728804e777dcd199a1d5b035b971f52
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-12-20 16:51:54 +0000

    Update

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # tna-newsletter
 
-## The National Archives Newsletter HTML Snippet
+## The National Archives Newsletter 
+
+### TNA-base
+* HTML form is in footer.php
+* Jquery plugin is in tna-global.js. The call to the plugin is in run-on-page-load.js
+* CSS is in footer-newsletter.scss
+* PHP function is in functions.php
+
+### TNA
+* HTML form is in footer.php
+* Jquery plugin is in tna-definitions.js, including the call to the plugin
+* CSS is in footer-newsletter.scss
--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
     $.fn.newsletterBackToOrigin = function (options) {
         var settings = $.extend({}, $.fn.newsletterBackToOrigin.defaults, options);
         return this.each(function () {
-            var thankYouURL = "http://nationalarchives.gov.uk/news/subscribe-confirmation.htm",
+            var thankYouURL = "http://test.nationalarchives.gov.uk/about/get-involved/newsletters/the-national-archives-newsletter/thank-you/",
                 newValue = "?oldurl=" + window.location.href;
             return settings.$element.val(thankYouURL + newValue);
         });
@@ -19,7 +19,3 @@
         $element: $('input[name="ReturnURL"]'),
     }
 }(jQuery));
-
-// Make sure the signup newsletter form matches the ID below
-// By default target element is $('input[name="ReturnURL"]')
-$('#signup').newsletterBackToOrigin();
